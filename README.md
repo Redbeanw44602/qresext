15426c5cb887462bf2c028d9196843d0f0e9f443
Redbeanw44602 <redbeana44945@gmail.com>
2025-11-12 23:20:26 +0800

    feat: may be usable.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # qresext
+
+```
+> qresext -o ./dump ./path/to/qtbin
+Read 10755096 bytes from /home/username/path/to/qtbin
+Executable Linkable Format (ELF) binary found.
+Itanium C++ ABI-compatible binary found.
+Qt resource triple found (struct=0x5f90a0, name=0x5f52d0, data=0x579e40).
+Registered to Qt resource system.
+313 file(s) dumped, approximately 1.58 MiB.
+```
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <argparse/argparse.hpp>
 
 #include "io/io.h"
+#include "qtutil/resource.h"
 #include "triple_finder/triple_finder.h"
 
 #include "config.h"
@@ -17,6 +18,8 @@ using namespace qresext;
 auto args_from_cmdline(int argc, char* argv[]) {
     struct {
         std::string filename;
+        std::string savepath;
+        std::string finder;
     } args;
 
     argparse::ArgumentParser program("qresext", VERSION);
@@ -27,28 +30,55 @@ auto args_from_cmdline(int argc, char* argv[]) {
         .help("Path to Qt binary executable.")
         .required();
 
+    program.add_argument("-o", "--output")
+        .help("Path to save directory.")
+        .required();
+
+    program.add_argument("-f", "--finder")
+        .help("Specify the triple(tree, names, payload) finder.")
+        .choices("lief")
+        .default_value("lief")
+        .implicit_value(true);
+
     // clang-format on
 
     program.parse_args(argc, argv);
 
     args.filename = program.get<std::string>("filename");
+    args.savepath = program.get<std::string>("--output");
+    args.finder   = program.get<std::string>("--finder");
 
     return args;
 }
 
 int main(int argc, char* argv[]) try {
+    using namespace triple_finder;
+    using namespace io;
+    using namespace qtutil;
+
     auto args = args_from_cmdline(argc, argv);
 
-    auto data = io::read_shared(args.filename);
-    if (!data) {
-        return 1;
-    }
+    // data must be available during the main() lifecycle.
+    auto data = read_shared(args.filename);
 
-    if (auto triple_finder = create(triple_finder_lief, *data)) {
-        triple_finder->find();
-    }
+    // clang-format off
+    auto result =
+        data.and_then([&args](const shared_byte_container_t& data) {
+                auto finder = triple_finder_lief;
+                return create(finder, *data);
+            })
+            .and_then([](const triple_finder_ref& finder) {
+                return finder->find();
+            })
+            .and_then([](const find_triple_result& triple) {
+                return register_resource_data(*triple);
+            })
+            .map([&args]() {
+                return dump_resources(args.savepath);
+            });
+    // clang-format off
 
-    return 0;
+    return result ? 0 : 1;
 } catch (const std::exception& e) {
     loge("{}", e.what());
     return 1;
